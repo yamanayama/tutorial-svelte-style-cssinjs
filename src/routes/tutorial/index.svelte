f39898bad8209a5866e132160b9b4221587ef897
kato.makoto <kato.makoto@H-2037noMacBook-Pro.local>
2020-02-04 20:02:33 +0900

    first commit

---
+++ src/routes/tutorial/index.svelte
@@ -0,0 +1,9 @@
+<script>
+import AppButton from '../../components/atoms/AppButton.svelte'
+import AppLabel from '../../components/atoms/AppLabel.svelte'
+import AppInput from '../../components/atoms/AppInput.svelte'
+</script>
+
+<AppLabel>エリア</AppLabel>
+<AppInput>エリア</AppInput>
+<AppButton>送信</AppButton>
